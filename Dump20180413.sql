5081aecc6f506b572a7998e3f8f7b08c60b4577f
eryoher <eryoher@gmail.com>
2018-04-13 18:34:32 -0500

    add sql file

---
+++ Dump20180413.sql
@@ -0,0 +1,113 @@
+-- MySQL dump 10.13  Distrib 5.7.21, for Linux (x86_64)
+--
+-- Host: localhost    Database: busCompany
+-- ------------------------------------------------------
+-- Server version	5.7.21-0ubuntu0.16.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `buses`
+--
+
+DROP TABLE IF EXISTS `buses`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `buses` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `model` varchar(455) COLLATE utf8_spanish_ci NOT NULL,
+  `license` text COLLATE utf8_spanish_ci,
+  `driver` varchar(455) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `buses`
+--
+
+LOCK TABLES `buses` WRITE;
+/*!40000 ALTER TABLE `buses` DISABLE KEYS */;
+INSERT INTO `buses` VALUES (1,'2014','MLZ321M','chofer',NULL),(2,'1967','TO093','Alberto',NULL);
+/*!40000 ALTER TABLE `buses` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `routes`
+--
+
+DROP TABLE IF EXISTS `routes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `routes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(455) COLLATE utf8_spanish_ci NOT NULL,
+  `city_origin` varchar(455) COLLATE utf8_spanish_ci NOT NULL,
+  `city_destiny` varchar(455) COLLATE utf8_spanish_ci NOT NULL,
+  `bus_id` int(11) DEFAULT NULL,
+  `value` float DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `unique_index` (`city_origin`,`city_destiny`)
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `routes`
+--
+
+LOCK TABLES `routes` WRITE;
+/*!40000 ALTER TABLE `routes` DISABLE KEYS */;
+INSERT INTO `routes` VALUES (1,'Ibague-Cali','Ibague','Cali',1,50000,NULL),(4,'Ibague-bogota','Ibague','Bogota',2,5,NULL),(5,'bogota-paipa','Bogota','Paipa',2,10,NULL),(6,'paipa-tunja','Paipa','Tunja',2,8,NULL),(7,'Bogota-tunja','Bogota','Tunja',2,8,NULL),(8,'Pasto-Ecuador','Pasto','Ecuador',1,15,NULL),(9,'Cali-Popayan','Cali','Popayan',1,15,NULL),(10,'Popayan-Ecuador','Popayan','Ecuador',1,10,NULL),(11,'Cali-Ibague','Cali','Ibague',2,50000,NULL),(18,'Medellin-Manizales','Medellin','Manizales',1,60000,NULL),(19,'Manizales-Medellin','Manizales','Medellin',1,60000,NULL);
+/*!40000 ALTER TABLE `routes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(455) COLLATE utf8_spanish_ci NOT NULL,
+  `lastname` varchar(455) COLLATE utf8_spanish_ci NOT NULL,
+  `username` text COLLATE utf8_spanish_ci,
+  `password` text COLLATE utf8_spanish_ci,
+  `created` datetime DEFAULT NULL,
+  `email` text COLLATE utf8_spanish_ci,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'Ericson yohany','hernandez franco','admin','5f4dcc3b5aa765d61d8327deb882cf99',NULL,'eryoher@gmail.com'),(4,'Cuarto','Prueba apellido','cuarto','202cb962ac59075b964b07152d234b70',NULL,'eryoher8@hotmail.com');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-04-13 18:31:37
